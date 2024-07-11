730a841e8ccf3da717cf5fb636b03cca0e2b355d
aliskhangazamatov <ag@farcana.com>
2024-07-11 23:56:24 +0300

    add logo, img

--- src/app/components/Main/Main.tsx
+++ src/app/components/Main/Main.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+import mainBg from "@/../public/assets/images/main-bg.jpg";
 
 const Main = () => {
-    return (
-        <div>
-            Main
-        </div>
-    );
+  return (
+    <div>
+      <Image src={mainBg} alt="main" />
+    </div>
+  );
 };
 
 export default Main;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/app/components/Header/Header";
+import Main from "@/app/components/Main/Main";
 
 export default function Home() {
   return (
-    <main className="xl:container mx-auto flex min-h-screen flex-col items-center justify-between px-5">
+    <main className="xl:container mx-auto flex flex-col items-center justify-between px-5">
       <Header />
+      <Main />
     </main>
   );
 }
