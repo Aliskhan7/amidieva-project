e0eaa91b0a368d5cd9f3c417d38b9276ba909ebd
aliskhangazamatov <ag@farcana.com>
2024-07-15 23:50:11 +0300

    add filter, shop page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Main from "@/app/components/Main/Main";
 import Products from "@/app/components/Products/Products";
+import React from "react";
 
 export default function Home() {
   return (
     <main>
       <Main />
-      <Products />
+      <section className="w-full mt-16">
+        <h2 className="title-h2 mb-10">Новинки</h2>
+        <Products />
+      </section>
     </main>
   );
 }
