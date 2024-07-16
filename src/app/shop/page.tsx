1a6df47ed0eaff10ef4a073be2c74726178500ed
aliskhangazamatov <ag@farcana.com>
2024-07-16 23:46:18 +0300

    add swicher, about page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -5,7 +5,7 @@ import Products from "@/app/components/Products/Products";
 export default function Shop() {
   return (
     <section className="w-full mt-16">
-      <h2 className="title-h2 mb-10">Новинки</h2>
+      <h2 className="title-h1 mb-10">Новинки</h2>
       <div className="grid grid-cols-4 gap-x-8">
         <Filter />
         <div className="col-span-3">
