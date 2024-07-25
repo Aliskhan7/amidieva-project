76d3ebe68a58f670a69821755662faf6325eab2a
aliskhangazamatov <ag@farcana.com>
2024-07-26 02:20:45 +0300

    create page cart

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div className="mt-24">
+      <h1 className="title-h1">Shopping Cart</h1>
+    </div>
+  );
+};
+
+export default Cart;
