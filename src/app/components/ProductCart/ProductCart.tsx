52a0bd55ab39dc5fdef9cfb7a51bbd374cbc6e2f
aliskhangazamatov <ag@farcana.com>
2024-07-28 00:09:19 +0300

    add design cart item

--- src/app/components/ProductCart/ProductCart.tsx
+++ src/app/components/ProductCart/ProductCart.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import { IProduct } from "@/app/types/types";
+import Counter from "@/app/components/UI/Counter";
 
-const ProductCart = () => {
+const ProductCart = (product: IProduct) => {
   return (
     <div className="w-[600px] mt-16 flex items-start gap-x-10">
-      <Image src="" alt="product" />
-      <div>
-        <p>Lira Earrings</p>
-        <span>Black / Medium</span>
-        <span>$ 20</span>
+      <Image width={136} height={136} src={product.url} alt="product" />
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col">
+          <p className="text-xl text-black mb-3.5">{product.title}</p>
+          <span className="text-base text-gray-400">Black / Medium</span>
+          <span className="text-base text-gold-400">$ {product.price}</span>
+        </div>
+        <div>
+          <Counter counter={4} />
+        </div>
       </div>
     </div>
   );
