aac9618f92faae1e60faca81f38dc2cf6f7d6be4
aliskhangazamatov <ag@farcana.com>
2024-07-30 20:27:29 +0300

    fix

--- src/app/components/ProductCart/ProductCart.tsx
+++ src/app/components/ProductCart/ProductCart.tsx
@@ -5,7 +5,7 @@ import Counter from "@/app/components/UI/Counter";
 
 const ProductCart = (product: IProduct) => {
   return (
-    <div className="w-[600px] mt-16 flex items-start gap-x-10">
+    <div className="w-[600px] flex items-start gap-x-10">
       <Image width={136} height={136} src={product.url} alt="product" />
       <div className="flex justify-between w-full">
         <div className="flex flex-col">
