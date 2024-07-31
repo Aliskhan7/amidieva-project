777501874573ccc3454ed93bda6de32a3914a12b
aliskhangazamatov <ag@farcana.com>
2024-07-31 22:14:22 +0300

    add styles

--- src/app/components/TotalsCart/TotalsCart.tsx
+++ src/app/components/TotalsCart/TotalsCart.tsx
@@ -4,23 +4,25 @@ const TotalsCart = () => {
   return (
     <div className="p-10">
       <h2 className="text-[26px] text-black">Cart totals</h2>
-      <div className="mt-11">
-        <div className="flex-between-center">
-          <span>SUBTOTAL</span>
-          <span>$ 65,00</span>
+      <div className="mt-11 ">
+        <div className="grid grid-cols-2 gap-x-32 mb-6">
+          <span className="text-base text-black">SUBTOTAL</span>
+          <span className="text-base text-gray-400">$ 65,00</span>
         </div>
-        <div className="flex justify-between items-start">
-          <span>SHIPPING</span>
-          <span>
+        <div className="grid grid-cols-2 gap-x-32 mb-6">
+          <span className="text-base text-black">SHIPPING</span>
+          <span className="text-base text-gray-400">
             Shipping costs will be calculated once you have provided address.
           </span>
         </div>
       </div>
-      <div className="flex-between-center">
-        <span>TOTAL</span>
-        <span>$ 87,00</span>
+      <div className="flex-between-center border-t border-t-gray-300 pt-11 mb-11">
+        <span className="text-base text-black font-bold">TOTAL</span>
+        <span className="text-base text-black font-bold">$ 87,00</span>
       </div>
-      <button>PROCEED TO CHECKOUT</button>
+      <button className="bg-black text-base text-white w-full py-4 rounded uppercase font-bold">
+        PROCEED TO CHECKOUT
+      </button>
     </div>
   );
 };
