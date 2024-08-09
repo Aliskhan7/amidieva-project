75552b4dadba4c6f9c213fafa26f346a9a18fe5a
aliskhangazamatov <ag@farcana.com>
2024-08-09 18:52:38 +0300

    add animate about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,27 +1,82 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import about_1 from "../../../public/assets/images/about_img_1.jpg";
 import about_2 from "../../../public/assets/images/about_img_2.jpg";
+import { motion } from "framer-motion";
 
 const About = () => {
+  const topAnimated = {
+    hidden: {
+      y: -100,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: { duration: 0.2, delay: 0.2 },
+    },
+  };
+
+  const bottomAnimated = {
+    hidden: {
+      y: 300,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: { duration: 0.2, delay: 0.2 },
+    },
+  };
+
+  const rightAnimated = {
+    hidden: {
+      x: 500,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: { duration: 0.2, delay: 0.2 },
+    },
+  };
+
+  const leftAnimated = {
+    hidden: {
+      x: -500,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: { duration: 0.2, delay: 0.2 },
+    },
+  };
   return (
-    <div className="w-[700px] mx-auto mt-24">
-      <div className="mb-10">
-        <div className="mb-12 text-center">
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      className="w-[700px] mx-auto mt-24"
+    >
+      <motion.div className="mb-10">
+        <motion.div variants={topAnimated} className="mb-12 text-center">
           <h1 className="title-h1 mb-6">About</h1>
           <p className="text-xl text-black">
             Who we are and why we do what we do!
           </p>
-        </div>
-        <p className="text-base text-black">
-          Duis rutrum dictum libero quis rutrum. Etiam sed neque aliquam,
-          sollicitudin ante a, gravida arcu. Nam fringilla molestie velit, eget
-          pellentesque risus scelerisque a. Nam ac urna maximus, tempor magna
-          et, placerat urna. Curabitur eu magna enim. Proin placerat tortor
-          lacus, ac sodales lectus placerat quis.
-        </p>
-      </div>
-      <div className="mb-10">
+        </motion.div>
+        <motion.div variants={bottomAnimated}>
+          <p className="text-base text-black">
+            Duis rutrum dictum libero quis rutrum. Etiam sed neque aliquam,
+            sollicitudin ante a, gravida arcu. Nam fringilla molestie velit,
+            eget pellentesque risus scelerisque a. Nam ac urna maximus, tempor
+            magna et, placerat urna. Curabitur eu magna enim. Proin placerat
+            tortor lacus, ac sodales lectus placerat quis.
+          </p>
+        </motion.div>
+      </motion.div>
+      <motion.div variants={rightAnimated} className="mb-10">
         <h2 className="text-[26px] text-black mb-6">Top trends</h2>
         <Image src={about_1} alt="photo" />
         <p className="text-base text-black mt-12">
@@ -34,8 +89,8 @@ const About = () => {
           <li>● consectetur adipiscing elit. Aliquam placerat</li>
           <li>● Lorem ipsum dolor sit amet consectetur </li>
         </ul>
-      </div>
-      <div>
+      </motion.div>
+      <motion.div variants={leftAnimated}>
         <h2 className="text-[26px] text-black mb-6">Produced with care</h2>
         <Image src={about_2} alt="photo" />
         <p className="text-base text-black mt-12">
@@ -45,8 +100,8 @@ const About = () => {
           consequat sed eu felis. Nunc sed porta augue. Morbi porta tempor odio,
           in molestie diam bibendu.
         </p>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
